95ec2cdcef31dc4859050a3c6bf95646f6e1d934
Irron Williams <irron.williams@gmail.com>
2020-03-08 14:43:23 -0700

    Update Objects_Maps.js

--- Objects_Maps.js
+++ Objects_Maps.js
@@ -224,7 +224,7 @@ console.log(getColor('orange'))  //returns #f60 because using [] notation
 delete colors['yellow Color'] //using delete operator to remove key value pair yellow Color from colors object.
 delete colors.blue  //using dot notation to remove blue key pair value from color object 
 
-/*EASY PROPERTY ACCESS WITH DESTRUCTURING: can leverage destructuring to get the specific properties that i need. 
+/*EASY PROPERTY ACCESS WITH DESTRUCTURING: can leverage destructuring to get the specific properties that i want/need. 
 
 program displays certain info/properties about user.*/
 const user = {
@@ -280,7 +280,7 @@ displayUser()
  const { title } = user.details
 */
 
-//this approach works but is using 2 separate destructuring statements for the same object users, repeating steps
+//this approach works but using 2 separate destructuring statements for the same object users, repeating steps
 const user = {
     name: "Reed",
     username: "Reedbarger",
@@ -328,3 +328,43 @@ function displayUserBio({ name, details: { title} }) { //replacing userData with
     console.log(`${name} is a ${title}`)  //returns Reed is a Programmer
   }  
 displayUserBio(user) 
+
+/*  Challenge: The recommendations object contains a set of of nice places to visit in Brighton, UK,
+organized by what you'd like to do (eat pancakes, drink coffee etc).
+
+1. Destructure the places to drink (coffee and beer) from recommendations
+2. Destructure the places to listen to music
+3. Write a function that takes the recommendations object as an argument and that
+     a) Logs out the music venues in recommendations when invoked 
+     b) Uses object descructuring to get the "traditional" and "jazz" keys from the argument
+*/
+
+const recommendations = {
+    pancakes: 'Nowhere Man',
+    riceBowls: 'Pompoko',
+    beer: 'The Craft Beer Co.',
+    coffee: 'Coffee Roasters',
+    small_plates: 'Venetian Plates',
+    music: { 
+        traditional: 'Fiddler\'s Elbow', 
+        jazz: 'The Paris House'
+    }
+}
+//Solution1: Destructure the places to drink (coffee and beer) from recommendations
+const { beer, coffee } = recommendations
+console.log(beer) //returns The Craft Beer Co.
+
+//Solution2: Destructure the places to listen to music
+const { traditional, jazz } = recommendations.music; //option 1
+console.log(jazz)  //returns The Paris House
+
+const { music: { traditional, jazz } } = recommendations; //option 2 tells JS objects traditional and jazz are nested in the music object
+console.log(jazz) //returns The Paris House
+
+//Solution 3
+function displayMusicPlaces({ music: { traditional, jazz } }) {
+    console.log(`Head to ${traditional} or ${jazz} to listen to great music!`)// returns Head to Fiddler's Elbow or The Paris House to listen to great music!
+}
+displayMusicPlaces(recommendations)
+
+
